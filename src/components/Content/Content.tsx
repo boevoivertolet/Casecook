10877665e3979dc97ce107e6b3703f74d8c436a3
boevoivertolet <bjha78rus@yandex.ru>
2023-02-09 20:14:30 +0300

    CSS

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,4 +1,46 @@
 import s from './Content.module.css'
 export const Content = () => {
-	return <div className={s.content}>Content</div>
+	return (
+		<div className={s.contentContainer}>
+			<div className={s.profileInfo}>
+				<img
+					className={s.wallPaper}
+					src='http://rdmkuhni.ru/images/cvet-kuhni/seraya-kuhnya/kuhnya-metallik-seryy.jpg'
+					alt='#'
+				/>
+				<div className={s.avaContainer}>
+					<div className={s.ava}>
+						<img
+							className={s.avaImg}
+							src='https://resizing.flixster.com/U7jLXZqIWp875Z4soUg1704DT78=/300x300/v2/https://flxt.tmsimg.com/assets/p13001485_i_h9_aa.jpg'
+							alt='#'
+						/>
+						<div className={s.nameDescription}>
+							<h2>Gordon Ramsey</h2>
+							<div>Status</div>
+							<div>Description</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className={s.postsFriendsContainer}>
+				<div className={s.postsContainer}>
+					<textarea
+						className={s.textarea}
+						placeholder="What's new ?"></textarea>
+					<div className={s.posts}>
+						<div className={s.postBody}>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Consectetur suscipit hic placeat nostrum natus enim.
+						</div>
+					</div>
+				</div>
+				<div className={s.friendsContainer}>
+					<div className={s.friends}>
+						<div className={s.friendsBody}>Friends Body </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,14 @@ import s from './Header.module.css'
 export const Header = () => {
 	return (
 		<div className={s.header}>
-			<img
-				className={s.logo}
-				src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Cc.logo.circle.svg/1024px-Cc.logo.circle.svg.png'
-				alt='logo'
-			/>
+			<div className={s.logoBlock}>
+				<img
+					className={s.logo}
+					src='https://creativecommons.org/images/deed/logo-cc-heart-white.png'
+					alt='logo'
+				/>
+				<span>Casecook</span>
+			</div>
 		</div>
 	)
 }
