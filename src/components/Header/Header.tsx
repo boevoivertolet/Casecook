92aeb0c1d4badb1390f0f944abf3b41a5d3ffde2
boevoivertolet <bjha78rus@yandex.ru>
2023-02-09 15:55:21 +0300

    Header Css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,12 @@
 import s from './Header.module.css'
 export const Header = () => {
-	return <div className={s.header}>Header</div>
+	return (
+		<div className={s.header}>
+			<img
+				className={s.logo}
+				src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Cc.logo.circle.svg/1024px-Cc.logo.circle.svg.png'
+				alt='logo'
+			/>
+		</div>
+	)
 }
