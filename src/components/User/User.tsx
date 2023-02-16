c58b1565b9bb8f55b619cb6b3062590945d6ca36
boevoivertolet <bjha78rus@yandex.ru>
2023-02-16 10:57:27 +0300

    userBlock CSS / Add button follow

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,18 +1,28 @@
 import { UsersItemsType } from '../../reducers/usersReducer'
 import s from './User.module.css'
+import userPhoto from '../../assets/userphoto/user.jpeg'
+import { UniversalButton } from '../../common/UniversalButton'
 export const User = (props: UserPropsType) => {
 	return (
 		<div className={s.userBlock}>
 			<div className={s.imgBlock}>
 				<img
 					className={s.img}
-					src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIS_dK1q17l-Oubnn1l3D6PJKljC7TPrB4rXQjUV7_h8qCg3Voifas3eGGxoldIweEC2E&usqp=CAU'
+					src={
+						props.user.photos.large ? props.user.photos.large : userPhoto
+					}
 					alt='#'
 				/>
 			</div>
 			<div className={s.statusName}>
-				<div>{props.user.photos.large}</div>
+				<div>{props.user.name}</div>
 				<div>{props.user.status}</div>
+				<UniversalButton
+					title={props.user.followed ? 'unfollow' : 'follow'}
+					onClick={() => {
+						alert('followed')
+					}}
+				/>
 			</div>
 		</div>
 	)
