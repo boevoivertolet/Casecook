1df50390ebcff104125c0327e38835c6b91837a0
boevoivertolet <bjha78rus@yandex.ru>
2023-03-07 16:06:02 +0300

    app class component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ import SettingsContainer from './components/Settings/SettingsContainer'
 import FriendsContainer from './components/Friends/FriendsContainer'
 import LoginContainer from './components/Login/LoginContainer'
 import GreetingsContainer from './components/Greetings/GreetingsContainer'
+import { ReduxRootStateType } from './store'
+import { Component } from 'react'
+import { getAuthUserData } from './reducers/authReducer'
+import { connect } from 'react-redux'
 
 // export const App = () => {
 // 	return (
@@ -44,37 +48,91 @@ import GreetingsContainer from './components/Greetings/GreetingsContainer'
 // 		</div>
 // 	)
 // }
-export const App = () => {
-	return (
-		<div className={s.app}>
-			<div className={s.header}>
-				<HeaderContainer />
+// export const App = () => {
+// 	return (
+// 		<div className={s.app}>
+// 			<div className={s.header}>
+// 				<HeaderContainer />
+// 			</div>
+// 			<div className={s.sidebar}>
+// 				<SidebarContainer />
+// 			</div>
+// 			<div className={s.content}>
+// 				<Routes>
+// 					<Route path={`/profile`}>
+// 						<Route index element={<ContentContainer />} />
+// 						<Route path=':userId' element={<ContentContainer />} />
+// 					</Route>
+// 					<Route path='/my-profile' element={<ContentContainer />} />
+
+// 					<Route path='/news' element={<NewsContainer />} />
+
+// 					<Route path='/messenger' element={<MessengerContainer />} />
+
+// 					<Route path='/music' element={<MusicContainer />} />
+
+// 					<Route path='/friends' element={<FriendsContainer />} />
+
+// 					<Route path='/settings' element={<SettingsContainer />} />
+
+// 					<Route path='/login' element={<LoginContainer />} />
+// 					<Route path='/casecook' element={<GreetingsContainer />} />
+// 				</Routes>
+// 			</div>
+// 		</div>
+// 	)
+// }
+class App extends Component<AppType> {
+	componentDidMount() {
+		this.props.getAuthUserData()
+	}
+	render() {
+		return (
+			<div className={s.app}>
+				<div className={s.header}>
+					<HeaderContainer />
+				</div>
+				<div className={s.sidebar}>
+					<SidebarContainer />
+				</div>
+				<div className={s.content}>
+					<Routes>
+						<Route path={`/profile`}>
+							<Route index element={<ContentContainer />} />
+							<Route path=':userId' element={<ContentContainer />} />
+						</Route>
+						<Route path='/my-profile' element={<ContentContainer />} />
+
+						<Route path='/news' element={<NewsContainer />} />
+
+						<Route path='/messenger' element={<MessengerContainer />} />
+
+						<Route path='/music' element={<MusicContainer />} />
+
+						<Route path='/friends' element={<FriendsContainer />} />
+
+						<Route path='/settings' element={<SettingsContainer />} />
+
+						<Route path='/login' element={<LoginContainer />} />
+						<Route path='/casecook' element={<GreetingsContainer />} />
+					</Routes>
+				</div>
 			</div>
-			<div className={s.sidebar}>
-				<SidebarContainer />
-			</div>
-			<div className={s.content}>
-				<Routes>
-					<Route path={`/profile`}>
-						<Route index element={<ContentContainer />} />
-						<Route path=':userId' element={<ContentContainer />} />
-					</Route>
-					<Route path='/my-profile' element={<ContentContainer />} />
-
-					<Route path='/news' element={<NewsContainer />} />
-
-					<Route path='/messenger' element={<MessengerContainer />} />
-
-					<Route path='/music' element={<MusicContainer />} />
-
-					<Route path='/friends' element={<FriendsContainer />} />
-
-					<Route path='/settings' element={<SettingsContainer />} />
-
-					<Route path='/login' element={<LoginContainer />} />
-					<Route path='/casecook' element={<GreetingsContainer />} />
-				</Routes>
-			</div>
-		</div>
-	)
+		)
+	}
 }
+
+const mapStateToProps = (state: ReduxRootStateType): AppMapStateToPropsType => {
+	return {
+		// authUser: state.auth.data
+	}
+}
+type AppMapStateToPropsType = {
+	// authUser: AuthUserType
+}
+type AppMapDispatchToPropsType = {
+	getAuthUserData: () => void
+}
+type AppType = AppMapDispatchToPropsType & AppMapStateToPropsType
+
+export default connect(mapStateToProps, { getAuthUserData })(App)
