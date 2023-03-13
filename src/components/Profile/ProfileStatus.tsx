c26c8be9d60a05ec8e79a4002396265543c78f71
boevoivertolet <bjha78rus@yandex.ru>
2023-03-13 17:07:30 +0300

    on Enter Press

--- src/components/Profile/ProfileStatus.tsx
+++ src/components/Profile/ProfileStatus.tsx
@@ -1,16 +1,27 @@
-import React, {ChangeEvent, useState} from 'react'
+import React, {ChangeEvent, KeyboardEvent, useEffect, useState} from 'react'
 import s from '../Content/Content.module.css'
 
 export const ProfileStatus = (props: ProfileStatusPropsType) => {
     const [status, setStatus] = useState<string>(props.status)
     const [editMode, setEditMode] = useState<boolean>(false)
 
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
+
+
     const activateEditMode = () => {
         setEditMode(true)
     }
     const deActivateEditMode = () => {
         setEditMode(false)
+        props.updateStatusProfile(status)
+    }
+    const onEnterDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            setEditMode(false)
             props.updateStatusProfile(status)
+        }
     }
 
     const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -23,13 +34,14 @@ export const ProfileStatus = (props: ProfileStatusPropsType) => {
             {!editMode && (
                 <div>
 						<span onDoubleClick={activateEditMode}>
-							{props.status || '----'}
+							{props.status || 'status'}
 						</span>
                 </div>
             )}
             {editMode && (
                 <div>
                     <input
+                        onKeyDown={onEnterDown}
                         onChange={onStatusChange}
                         autoFocus
                         onBlur={deActivateEditMode}
